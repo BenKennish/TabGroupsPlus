c2484e275226bd3d44eaf6daec2e4a9d91e8def4
Ben Kennish <ben@kennish.net>
2025-03-06 15:00:04 +0000

    Cleaner content script, fix async  isContentScriptActive, autoinject, etc!

--- content.js
+++ content.js
@@ -1,30 +1,37 @@
-/*
-if (window.hasRunTGPContentScript)
+// does this property of window get reset when the script is uninjected?
+if (!window.isInjected)
 {
-    // Already injected, so do nothing.
-    return;
-}
-window.hasRunTGPContentScript = true;
-*/
+    window.isInjected = true;
 
-// Function to handle mouseenter event
-function handleMouseEnter()
-{
-    // send message to the background script
-    chrome.runtime.sendMessage({ action: 'mouseInContentArea', value: true });
-}
-
-document.documentElement.addEventListener('mouseenter', handleMouseEnter);
-
-
-// ping handler for checking whether the content script is hooked into the current tab
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>
-{
-    if (message.action === "ping")
+    window.addEventListener('beforeunload', () =>
     {
-        sendResponse({ status: "ok" });
+        // clean up just before window is unloaded
+        window.isInjected = false;
+        document.documentElement.removeEventListener('mouseenter', handleMouseEnter);
+    });
+
+    // Function to handle mouseenter event
+    function handleMouseEnter()
+    {
+        console.log('[TabGroupsPlus] Mouse entered content area - sending message to background script');
+        chrome.runtime.sendMessage({ action: 'mouseInContentArea', value: true });
     }
-});
 
+    document.documentElement.addEventListener('mouseenter', handleMouseEnter);
 
-console.log('[TabGroupsPlus] Content script loaded');
+    // handler for pings from background.js,
+    // used to check whether the content script is hooked into the current tab
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>
+    {
+        if (window.isInjected && message.action === "ping")
+        {
+            sendResponse({ status: "ok" });
+        }
+    });
+
+    console.log('[TabGroupsPlus] Content script loaded');
+}
+else
+{
+    console.log('[TabGroupsPlus] Content script already loaded');
+}
